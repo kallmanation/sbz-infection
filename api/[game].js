4b0c6c182f36acb69b01648a6c7707d3293114ac
Nathan Kallman <nathan@joinroot.com>
2022-04-07 14:53:59 -0400

    Towards functioning event to game object projection

--- api/[game].js
+++ api/[game].js
@@ -3,15 +3,27 @@ import { randomReference } from './_utils/references'
 const faunadb = require('faunadb'), q = faunadb.query
 
 module.exports = (req, res) => {
+  // TODO: DRY player finding?
+  const player = req.cookies.SbzPlayerRef || randomReference();
   // TODO: move fauna stuff to a single place
   const store = new faunadb.Client({ secret: process.env.FAUNA_DB_KEY, domain: 'db.us.fauna.com', keepAlive: false })
+  // const createP = store.query(
+  //   q.Get(
+  //     q.Match(q.Index('index_game_ref'), req.query.game)
+  //   )
+  // );
   const createP = store.query(
-    q.Get(
-      q.Match(q.Index('index_game_ref'), req.query.game)
+    q.Map(
+      q.Paginate(
+        q.Match(q.Index('index_game_ref'), req.query.game)
+      ),
+      q.Lambda("X", q.Get(q.Var("X")))
     )
   );
 
   createP.then((response) => {
-    res.status(200).json(response.data);
+    res.status(200)
+       .setHeader('Set-Cookie', `SbzPlayerRef=${player}; HttpOnly`)
+       .json(response.data);
   });
 };
--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
 import { randomReference } from './_utils/references'
+import { events } from './_utils/game_projector'
 
 const faunadb = require('faunadb'), q = faunadb.query
 
 module.exports = (req, res) => {
-  // TODO: move events to a single place
+  // TODO: DRY player finding?
   const player = req.cookies.SbzPlayerRef || randomReference();
+  // TODO: move events to a single place
   const newGameEvent = {
     game_ref: randomReference(),
     sent_by: player,
     event_ref: randomReference(),
-    event_type: 'new_game',
+    event_type: events.NEW_GAME,
     nickname: req.body.nickname
   };
 
@@ -23,8 +25,6 @@ module.exports = (req, res) => {
   );
 
   createP.then((response) => {
-    // res.status(200).send(`Hello ${process.env.FAUNA_DB_KEY} -  ${JSON.stringify(response)} - ${JSON.stringify(newGameEvent)}!`);
-    // res.status(200).send(`<html><head><title>Creating Game</title></head><body>${JSON.stringify(response)}</body></html>`);
     res.status(303)
        .setHeader('Location', `/api/${newGameEvent.game_ref}`)
        .setHeader('Set-Cookie', `SbzPlayerRef=${player}; HttpOnly`)
