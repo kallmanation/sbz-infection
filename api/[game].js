c3b87571b9866c04237f13902af1bfce10a2afec
Nathan Kallman <nathan@joinroot.com>
2022-04-08 16:23:54 -0400

    Add basic templating via RSRC and start game lobby

--- api/[game].js
+++ api/[game].js
@@ -1,19 +1,34 @@
 import { randomReference } from './_utils/references'
+import { events } from './_utils/game_projector'
 import { project_from_planetscale } from './_utils/game_projector'
+import { readFile } from 'fs/promises'
 
 export default async (req, res) => {
   // TODO: DRY player finding?
-  // const player = req.cookies.SbzPlayerRef || randomReference();
+  const player = req.cookies.SbzPlayerRef || randomReference();
 
-  // TODO: common DB
-  // const connection = await mysql.createConnection(`mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process.env.PLANETSCALE_DB_PASSWORD}@${process.env.PLANETSCALE_DB_HOST}/${process.env.PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`);
-  // const [rows, fields] = await connection.execute(
-  //   "SELECT * FROM `game_events` WHERE game_ref = ?",
-  //   [req.query.game]
-  // );
+  const projection = project_from_planetscale(req.query.game);
+  const head_template = readFile(`${__dirname}/_templates/head.html`);
+  const fade_message_template = readFile(`${__dirname}/_templates/fade_message.html`);
+  const game_form_template = readFile(`${__dirname}/_templates/game_form.html`);
+  const game_state = await projection;
 
-  const game_state = await project_from_planetscale(req.query.game);
+  const player_list = Object.values(game_state['players']);
 
-  res.status(200);
-  res.send(game_state);
+  if (player in game_state['players']) {
+    res.status(200);
+    res.send(
+      (await head_template).toString() +
+      (await fade_message_template).toString() +
+      (await game_form_template).toString() +
+      '<fade-message>Waiting for players to join...</fade-message>' +
+      '<h2>Players:</h2>' +
+      `<ul>${player_list}</ul>` +
+      `<game-form><input slot="inputs" type="hidden" name="type" value="${events.BEGIN_GAME}" /><span slot="submit">Ready</span></game-form>`
+    );
+  } else {
+    res.status(200);
+    res.setHeader('Set-Cookie', `SbzPlayerRef=${player}; HttpOnly`);
+    res.send('I do not recognize you. Have a cookie.');
+  }
 };
